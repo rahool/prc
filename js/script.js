709b2fbfa038558e925f7afb3bed6aa12c04473f
Rahul Yadava <rahool@gmail.com>
2017-02-20 02:59:40 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	// create the module and name it prcApp
+	var prcApp = angular.module('prcApp', ['ngRoute']);
 
 	// configure our routes
-	scotchApp.config(function($routeProvider) {
+	prcApp.config(function($routeProvider) {
 		$routeProvider
 
 			// route for the home page
@@ -37,23 +37,23 @@
 	});
 
 	// create the controller and inject Angular's $scope
-	scotchApp.controller('mainController', function($scope) {
+	prcApp.controller('mainController', function($scope) {
 		// create a message to display in our view
 		$scope.message = '';
 	});
 
-	scotchApp.controller('aboutController', function($scope) {
+	prcApp.controller('aboutController', function($scope) {
 		$scope.message = '';
 	});
 
-	scotchApp.controller('servicesController', function($scope) {
+	prcApp.controller('servicesController', function($scope) {
 		$scope.message = '';
 	});
 
-	scotchApp.controller('productsController', function($scope) {
+	prcApp.controller('productsController', function($scope) {
 		$scope.message = 'Our products are engineered as a direct replacement from the factory manifold. Outstation customers need not worry as they are of simple hassle-free bolt-on installation that can be fitted by anyone.';
 	});
 
-	scotchApp.controller('contactController', function($scope) {
+	prcApp.controller('contactController', function($scope) {
 		$scope.message = '';
 	});
